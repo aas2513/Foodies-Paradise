eac6e12f58501d003fa1feef20e2c87b369e7801
aas2513 <60225147+aas2513@users.noreply.github.com>
2020-12-02 18:33:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+let btnadd1=document.querySelector('#add1');
+let btnsubtract1=document.querySelector('#subtract1');
+let input1=document.querySelector('#input1'); 
+btnadd1.addEventListener('click', () => {  input1.value = parseInt(input1.value) +1;   });
+btnsubtract1.addEventListener('click', () => {  input1.value = parseInt(input1.value) -1;     });
+
+let btnadd2=document.querySelector('#add2');
+let btnsubtract2=document.querySelector('#subtract2');
+let input2=document.querySelector('#input2'); 
+btnadd2.addEventListener('click', () => {  input2.value = parseInt(input2.value) +1;   });
+btnsubtract2.addEventListener('click', () => {  input2.value = parseInt(input2.value) -1;     });
+
+let btnadd3=document.querySelector('#add3');
+let btnsubtract3=document.querySelector('#subtract3');
+let input3=document.querySelector('#input3'); 
+btnadd3.addEventListener('click', () => {  input3.value = parseInt(input3.value) +1;   });
+btnsubtract3.addEventListener('click', () => {  input3.value = parseInt(input3.value) -1;     });
+
+let btnadd4=document.querySelector('#add4');
+let btnsubtract4=document.querySelector('#subtract4');
+let input4=document.querySelector('#input4'); 
+btnadd4.addEventListener('click', () => {  input4.value = parseInt(input4.value) +1;   });
+btnsubtract4.addEventListener('click', () => {  input4.value = parseInt(input4.value) -1;     });
+
+let btnadd5=document.querySelector('#add5');
+let btnsubtract5=document.querySelector('#subtract5');
+let input5=document.querySelector('#input5'); 
+btnadd5.addEventListener('click', () => {  input5.value = parseInt(input5.value) +1;   });
+btnsubtract5.addEventListener('click', () => {  input5.value = parseInt(input5.value) -1;     });
+
+let btnadd6=document.querySelector('#add6');
+let btnsubtract6=document.querySelector('#subtract6');
+let input6=document.querySelector('#input6'); 
+btnadd6.addEventListener('click', () => {  input6.value = parseInt(input6.value) +1;   });
+btnsubtract6.addEventListener('click', () => {  input6.value = parseInt(input6.value) -1;     });
+
+let btnadd7=document.querySelector('#add7');
+let btnsubtract7=document.querySelector('#subtract7');
+let input7=document.querySelector('#input7'); 
+btnadd7.addEventListener('click', () => {  input7.value = parseInt(input7.value) +1;   });
+btnsubtract7.addEventListener('click', () => {  input7.value = parseInt(input7.value) -1;     });
+
+let btnadd8=document.querySelector('#add8');
+let btnsubtract8=document.querySelector('#subtract8');
+let input8=document.querySelector('#input8'); 
+btnadd8.addEventListener('click', () => {  input8.value = parseInt(input8.value) +1;   });
+btnsubtract8.addEventListener('click', () => {  input8.value = parseInt(input8.value) -1;     });
+
+let btnadd9=document.querySelector('#add9');
+let btnsubtract9=document.querySelector('#subtract9');
+let input9=document.querySelector('#input9'); 
+btnadd9.addEventListener('click', () => {  input9.value = parseInt(input9.value) +1;   });
+btnsubtract9.addEventListener('click', () => {  input9.value = parseInt(input9.value) -1;     });
+
+let btnadd10=document.querySelector('#add10');
+let btnsubtract10=document.querySelector('#subtract10');
+let input10=document.querySelector('#input10'); 
+btnadd10.addEventListener('click', () => {  input10.value = parseInt(input10.value) +1;   });
+btnsubtract10.addEventListener('click', () => {  input10.value = parseInt(input10.value) -1;     });
